3a2a3def7e6cf77e59e63b76376a8483907b2adf
Mshawver8 <flauchic@gmail.com>
2024-06-27 19:36:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ function showResponse() {
                 "What's your part in this?",
                 "Have you done a fourth step?",
                 "All you have to do is keep your side of the street clean ",
-                "Pray, drink a glass of water  eat something, and take a shower  and call me back."
-                "Pray, do some dishes, and call me back."
-          "Reach out to another alcoholic."
+                "Pray, drink a glass of water  eat something, and take a shower, and call me back.",
+                "Pray, do some dishes, and call me back.",
+          "Reach out to another alcoholic.",
           "Who can you help?"
     ];
     var response = responses[Math.floor(Math.random() * responses.length)];
