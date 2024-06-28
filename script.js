4381760807899f1de0d521c0968ba48306a6e5fc
Mshawver8 <flauchic@gmail.com>
2024-06-27 19:45:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 function showResponse() {
     var responses = [
-        "Yes, definitely!",
-        "No, not a chance.",
-        "Ask again later.",
-        "It is certain.",
-        "Better not tell you now.",
-        "Concentrate and ask again.",
-        "Very doubtful.",
-        "Outlook good."
+        "Have you prayed about it?",
+
+"Have you been to a meeting today?",
+
+"Pray, read pages 86-88, and call me back ",
+
+"You can't feel your way into acting; you have to act your way into feeling.",
+
+"What's your part in this?",
+
+"Have you done a fourth step on this?",
+
+"All you have to do is keep your side of the street clean.",
+
+"Pray, drink a glass of water eat something, and call me back.",
+
+"Pray, do some dishes, and call me back.",
+
+"Reach out to another alcoholic.",
+
+"Have you thought about someone you can help?"
     ];
     var response = responses[Math.floor(Math.random() * responses.length)];
     document.getElementById('answer').innerText = response;
